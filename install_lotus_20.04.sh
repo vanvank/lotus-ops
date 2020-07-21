1e7d92bf454a9bd68b5fcdf6e0de23ed4bdaa438
vanvank <165584943@qq.com>
2020-07-22 00:42:00 +0800

    Update install_lotus_20.04.sh

--- install_lotus_20.04.sh
+++ install_lotus_20.04.sh
@@ -12,7 +12,7 @@ sudo apt install mesa-opencl-icd ocl-icd-opencl-dev -y && \
 ##  sudo add-apt-repository ppa:longsleep/golang-backports -y && \
 sudo apt update && \
 ##  sudo apt install curl golang-go gcc git bzr jq pkg-config  -y && \
-sudo apt install curl  gcc git bzr jq pkg-config  -y && \
+sudo apt install curl  gcc make git bzr jq pkg-config  -y && \
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
 source $HOME/.cargo/env
 
