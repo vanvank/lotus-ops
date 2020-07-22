e18e9b2a7f2c548e979c4e25c06c542bf87ea58d
vanvank <165584943@qq.com>
2020-07-22 11:29:30 +0800

    Update update_lotus.sh

--- update_lotus.sh
+++ update_lotus.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # user
-
+echo -n "pls enter the branch (enter for master): "
+read branch
+if [ -n $branch ];then
+branch=master
+fi
 cd ~
 rm -rf lotus/
-git clone https://github.com/filecoin-project/lotus.git
+git clone --branch=$branch https://github.com/filecoin-project/lotus.git
 cd lotus/
 #git checkout testnet/3
 #git checkout interopnet
