102f7d356f7c18243a5f820a3d193f344e025aba
vanvank <165584943@qq.com>
2020-07-22 14:50:02 +0800

    Update get_api_token.sh

--- get_api_token.sh
+++ get_api_token.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-$1=user
-$2=host
-echo "要获取lotus的API和TOKEN请输入1，获取miner的API和TOKEN请输入2 "
-read choose
+user=$1
+host=$2
+# choose: 1 for lotus api, 2 for miner api(default)
+choose=$3
+
+if [ -z $3 ];then
+choose=2
+fi
+
+echo $choose
+
 if [ $choose == 1 ];then
 dir=/lotus_data/.lotus
 elif [ $choose == 2 ];then
