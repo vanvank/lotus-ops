0f062388eba441bb4aa25ded576ec93663cd6eb0
vanvank <165584943@qq.com>
2020-07-25 00:22:28 +0800

    Create cache_count.sh

---
+++ monitor/cache_count.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# count num of sectors  and total size in dir /lotus_data/.lotusworker/cache
+
+set -x
+max_job=3
+while true;do
+    cache_count=$(ls /lotus_data/.lotusworker/cache|wc -l)
+    echo $(date)
+    echo $cache_count
+    worker_dir_size=$(du -sh /lotus_data/.lotusworker| awk '{print $1}')
+    echo $worker_dir_size
+    if [ $cache_count -ge $max_job ]
+    then
+        echo send message
+        ./send_msg.sh "$HOSTNAME cache count: $cache_count, worker_repo size: $worker_dir_size"
+        sleep 1800
+    fi
+echo ---
+sleep 300
+done
