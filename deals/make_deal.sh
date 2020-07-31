42c24071eeeff2224e5451bd2e5744e208c89eec
vanvank <165584943@qq.com>
2020-07-31 14:15:23 +0800

    Update make_deal.sh

--- deals/make_deal.sh
+++ deals/make_deal.sh
@@ -6,6 +6,7 @@ count=${2:-1}
 filename=`date +%m-%d-%H-%M-%S`
 dd if=/dev/zero of=$filename count=$count bs=1M
 CID=`lotus client import $filename|awk '{print $4}'`
-echo $CID
+echo "文件CID是： $CID"
 date
+echo -n "DealCID 是： "
 lotus client deal $CID $miner_id 0.0000000005 622080
