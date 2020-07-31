94ea89ace8aa9c610c0fd68a81b2f6a61b4e15b6
vanvank <165584943@qq.com>
2020-07-31 11:35:22 +0800

    Update make_deal.sh

--- deals/make_deal.sh
+++ deals/make_deal.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 miner_id=$1
+count=${2:-1}  
 
 filename=`date +%m-%d-%H-%M-%S`
-dd if=/dev/zero of=$filename count=10 bs=1M
+dd if=/dev/zero of=$filename count=$count bs=1M
 CID=`lotus client import $filename|awk '{print $4}'`
 echo $CID
 date
