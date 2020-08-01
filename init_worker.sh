e82a295df731aaf0aede2671bd353c4ca3d73e34
vanvank <165584943@qq.com>
2020-08-01 20:47:50 +0800

    Update init_worker.sh

--- init_worker.sh
+++ init_worker.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 branch=ntwk-calibration
 user=psdz
+#user=muse-01
 miner_ip=192.168.11.48
+iner_ip=192.168.0.30
 pre1="true" 
 pre2="true"
 commit="true" 
 gpu="false"
+#gpu="true"
 
 rm -rf /lotus_data/.lotus*
 ./get_api_token.sh $user $miner_ip && \
