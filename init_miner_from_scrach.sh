601e0b0f40ba5e028298e92fad052bc430188385
vanvank <165584943@qq.com>
2020-08-01 17:33:27 +0800

    Create init_miner_from_scrach.sh

---
+++ init_miner_from_scrach.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+branch=ntwk-calibration
+user=xl
+daemon_ip=192.168.11.102
+
+rm -rf /lotus_data/.lotus*
+./get_api_token.sh $user $daemon_ip 1
+./update_lotus.sh $branch && \
+cd ~
+lotus-miner init --owner $( lotus wallet list | grep -m1 '^t3' ) --sector-size 32GiB
