22f7433eeef2fdf0ad3273b4a507453992a1c95a
vanvank <165584943@qq.com>
2020-08-04 10:49:18 +0800

    Update stop_worker.sh

--- scripts/stop_worker.sh
+++ scripts/stop_worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ps aux|grep lotus-worker |grep -v grep|awk '{print$2}'|xargs kill
+ps aux|grep lotus-worker |grep -v grep|awk '{print$2}'|xargs kill >  /dev/null 2>&1
 sleep 3
 ps aux|grep lotus-worker |grep -v grep|awk '{print$2}'|wc -l
