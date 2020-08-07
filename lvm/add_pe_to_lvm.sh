933430c4248ec11bdf6183384f42ae25f9e971b1
vanvank <165584943@qq.com>
2020-08-07 16:57:46 +0800

    Update add_pe_to_lvm.sh

--- lvm/add_pe_to_lvm.sh
+++ lvm/add_pe_to_lvm.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # vg中如果还有pe没分配，可用这个命令将剩余pe分配完
 
+logon_user=$(whoami)
+if [[ logon_user != root ]];then
+    echo "请用root执行此脚本"
+    exit 1
+fi
+
 vgscan
 echo -n "请输入卷组名(默认vgubuntu)："
 read vgname
