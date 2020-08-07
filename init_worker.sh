f23f3e3268de0a39f43a937d9278ac68a0449313
vanvank <165584943@qq.com>
2020-08-08 00:24:56 +0800

    Update init_worker.sh

--- init_worker.sh
+++ init_worker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-branch=ntwk-calibration
-user=psdz
+#branch=ntwk-calibration
+#user=psdz
 #user=muse-01
-miner_ip=192.168.11.48
-iner_ip=192.168.0.30
+#miner_ip=192.168.11.48
+#miner_ip=192.168.0.30
+
+source /etc/filecoin-mining.config
+
 pre1="true" 
 pre2="true"
 commit="true" 
@@ -11,7 +14,7 @@ gpu="false"
 #gpu="true"
 
 rm -rf /lotus_data/.lotus*
-./get_api_token.sh $user $miner_ip && \
+./get_api_token.sh $miner_login_name $miner_internal_ip && \
 ./update_lotus.sh $branch && \
 ./gen_start_worker.py $pre1 $pre2 $commit $gpu
 cd ~
