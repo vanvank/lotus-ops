0a922f03614db6e69edbe61fadb79e4483228f0d
vanvank <165584943@qq.com>
2020-08-08 00:42:19 +0800

    Update 1install_dep_and_lotus.sh

--- 1install_dep_and_lotus.sh
+++ 1install_dep_and_lotus.sh
@@ -3,11 +3,11 @@ version=cat /etc/issue|grep -i ubuntu|awk '{print substr($2,0,5)}'
 
 ./install_config
 
-if [[ $version == 18.04 ]];then
+if [[ $version == '18.04' ]];then
     ./install_lotus_18.04.sh
 fi
 
-if [[ $version == 20.04 ]];then
+if [[ $version == '20.04' ]];then
     ./install_lotus_20.04.sh
 fi
 
