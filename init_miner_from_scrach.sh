7cae15a83233cdc04ad7e15047c0ba7ab23fe445
vanvank <165584943@qq.com>
2020-08-08 00:28:01 +0800

    Update init_miner_from_scrach.sh

--- init_miner_from_scrach.sh
+++ init_miner_from_scrach.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-branch=ntwk-calibration
-user=xl
-daemon_ip=192.168.11.102
+#branch=ntwk-calibration
+#user=xl
+#daemon_ip=192.168.11.102
+source /etc/filecoin-mining.config
 
 rm -rf /lotus_data/.lotus*
-./get_api_token.sh $user $daemon_ip 1
+./get_api_token.sh $daemon_login_name $daemon_ip 1
 ./update_lotus.sh $branch && \
 cd ~
 lotus-miner init --owner $( lotus wallet list | grep -m1 '^t3' ) --sector-size 32GiB
