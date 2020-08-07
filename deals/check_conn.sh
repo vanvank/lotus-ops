1ec33567b0bc01ec5085e0b7015db2f2a5485e61
vanvank <165584943@qq.com>
2020-08-07 14:49:42 +0800

    Update check_conn.sh

--- deals/check_conn.sh
+++ deals/check_conn.sh
@@ -3,15 +3,28 @@
 
 miner_id=$1
 
+# 获取自己的miner_id
 if [ -z $1 ];then
     miner_id=`lotus-miner info|grep Miner:|awk '{print $2}'`
 fi
 
+# 查询链上信息，是否设置了multi addr， 如果没有请手动设置lotus-miner actor set-addrs --gas-limit 5000000 /ip4/$public_ip/tcp/$port
 echo "lotus state miner-info"
 lotus state miner-info $miner_id
 
+echo -n "链上是否已经设置了multi address：（y/n）:"
+read is_set
+if [[ is_set == n ]];then
+    echo -n "请输入你的公网ip： "
+    read public_ip
+    echo -n "请输入你的libp2p端口号： "
+    read port
+    echo "正在设置mult addr上链"
+    lotus-miner actor set-addrs --gas-limit 5000000 /ip4/$public_ip/tcp/$port
+fi
+
 echo '***'
-echo "lotus client query-ask"
+echo "lotus client query-ask miner_id, 这条命令最好在其他机器上执行"
 lotus client query-ask $miner_id
 
 echo '***'
