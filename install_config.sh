1c666fd983db0aa7251ab95c2314a8474c7b4fc2
vanvank <165584943@qq.com>
2020-08-08 00:40:44 +0800

    Update install_config.sh

--- install_config.sh
+++ install_config.sh
@@ -3,3 +3,5 @@ if [[ ! -f /etc/filecoin-mining.config ]];then
     echo "generate /etc/filecoin-mining.config"
     sudo cp ./config/filecoin-mining.config.sample /etc/filecoin-mining.config
 fi
+cat /etc/filecoin-mining.config
+echo
