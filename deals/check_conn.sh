e6ad4dc589bc7bf7ca082d88acbd7a120db162d0
vanvank <165584943@qq.com>
2020-08-07 14:42:33 +0800

    Update check_conn.sh

--- deals/check_conn.sh
+++ deals/check_conn.sh
@@ -13,6 +13,13 @@ lotus state miner-info $miner_id
 echo '***'
 echo "lotus client query-ask"
 lotus client query-ask $miner_id
+
 echo '***'
 echo "lotus-miner net peers: "
-lotus-miner net peers|wc -l
+net_peers=$(lotus-miner net peers|wc -l)
+echo $net_peers
+
+if [[ $net_peers -lt 3 ]];then
+    echo "连接数太少，尝试连接bootstrap"
+    ./miner_conn.sh
+fi
