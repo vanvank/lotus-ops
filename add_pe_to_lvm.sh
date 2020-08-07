77652ba9bcc7052372ee6a666f1a9ffb84518d48
vanvank <165584943@qq.com>
2020-08-07 15:40:05 +0800

    Update add_pe_to_lvm.sh

--- add_pe_to_lvm.sh
+++ add_pe_to_lvm.sh
@@ -2,8 +2,12 @@
 # vg中如果还有pe没分配，可用这个命令将剩余pe分配完
 
 vgscan
-echo -n "请输入卷组名："
+echo -n "请输入卷组名(默认vgubuntu)："
 read vgname
+
+if [[ -z $vgname ]];then
+        vgname=vgubuntu
+fi
 
 free_pe=$(vgdisplay|grep Free|awk '{print $5}')
 if [[ $free_pe -gt 0 ]];then
