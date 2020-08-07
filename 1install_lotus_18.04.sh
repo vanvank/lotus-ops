1542ef6a3f63a48a5915081823b32d0cc00082b4
vanvank <165584943@qq.com>
2020-08-07 10:32:00 +0800

    Update 1install_lotus_18.04.sh

--- 1install_lotus_18.04.sh
+++ 1install_lotus_18.04.sh
@@ -8,6 +8,7 @@ sudo apt update && \
 sudo apt install curl golang-go gcc make git bzr jq pkg-config  -y && \
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
 source $HOME/.cargo/env
+source /etc/profile
 
 # pull code and make lotus
 ./update_lotus.sh
