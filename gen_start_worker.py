be75cf8c7db67a0d88bec2f6f02f1986ad0d4c79
vanvank <165584943@qq.com>
2020-08-09 09:22:53 +0800

    Update gen_start_worker.py

--- gen_start_worker.py
+++ gen_start_worker.py
@@ -18,7 +18,7 @@ def gen_file(pre1="true",pre2="true",commit="true", gpu="false"):
     home_dir = os.path.expanduser('~')
     start_file = os.path.join(home_dir,"start_worker.sh")
     with open(start_file, "w") as f:
-        line = "nohup lotus-worker --enable-gpu-proving=%s run --address %s:2346  --precommit1=%s --precommit2=%s --commit=%s > worker.log 2>&1 &" % (gpu, worker_ip, pre1, pre2,commit)
+        line = "nohup lotus-worker --enable-gpu-proving=%s run --address %s:2346  --precommit1=%s --precommit2=%s --commit=%s >> worker.log 2>&1 &" % (gpu, worker_ip, pre1, pre2,commit)
         f.write("#!/bin/bash"+"\n")
         f.write(line + "\n")
     os.chmod(start_file,stat.S_IRWXU)
