dd8ffde967c3f92796beac12af555e8f83f968c2
vanvank <165584943@qq.com>
2020-08-10 22:08:27 +0800

    Update cache_count.sh

--- monitor/cache_count.sh
+++ monitor/cache_count.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-# count num of sectors  and total size of dir:/lotus_data/.lotusworker/cache
 
-set -x
-max_job=2
+max_job=8
+
 while true;do
-    cache_count=$(ls /lotus_data/.lotusworker/cache|wc -l)
-    echo $(date)
-    echo $cache_count
-    worker_dir_size=$(du -sh /lotus_data/.lotusworker| awk '{print $1}')
-    echo $worker_dir_size
-    if [ $cache_count -gt $max_job ]
-    then
-        echo send message
-        ./send_msg.sh "$HOSTNAME cache count: $cache_count, worker_repo size: $worker_dir_size"
-        sleep 1800
-    fi
-echo ---
-sleep 300
+    date
+    echo ---begin---------------------------------------------------------------------------
+    cache_count=$(ls $WORKER_PATH/cache|grep ^s|wc -l)
+    echo "cache count: $cache_count"
+    du -sh $WORKER_PATH/cache/*
+    echo ---
+    du -sh $WORKER_PATH/unsealed/*
+    echo ---
+    du -sh $WORKER_PATH/sealed/*
+    echo ---
+    du -sh $WORKER_PATH/*
+    worker_dir_size=$(du -sh $WORKER_PATH| awk '{print $1}')
+    echo "total: $worker_dir_size"
+    uptime
+    free -h
+    #if [ $cache_count -gt $max_job ]
+    #then
+    #    echo send message
+        #./send_msg.sh "$HOSTNAME cache count: $cache_count, worker_repo size: $worker_dir_size"
+    #    sleep 1800
+    #fi
+    echo ---end--------------------------------------------------------------------------------
+    sleep 300
 done
