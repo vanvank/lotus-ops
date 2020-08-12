8440f404c79fd1c9953b3dccdf232fbbafad9759
vanvank <165584943@qq.com>
2020-08-12 14:59:49 +0800

    Update init_worker.sh

--- init_worker.sh
+++ init_worker.sh
@@ -13,6 +13,15 @@ commit="true"
 gpu="false"
 #gpu="true"
 
+if [[ $gpu == "true" ]];then
+    sed -i '/FIL_PROOFS_USE_GPU_TREE_BUILDER/d' /etc/profile
+    echo 'export FIL_PROOFS_USE_GPU_TREE_BUILDER=1' >> /etc/profile
+
+    sed -i '/FIL_PROOFS_USE_GPU_COLUMN_BUILDER/d' /etc/profile
+    echo 'export FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1' >> /etc/profile
+fi
+
+source /etc/profile
 rm -rf /lotus_data/.lotus*
 ./get_api_token.sh $miner_login_name $miner_internal_ip && \
 ./update_lotus.sh $branch && \
