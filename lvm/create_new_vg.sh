026041dac5603e147ecaeca4738eb34583930c0d
vanvank <165584943@qq.com>
2020-10-17 23:31:31 +0800

    Create create_new_vg.sh

---
+++ lvm/create_new_vg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+set -x
+disk=$1
+vgname=$2
+
+echo $1 |grep nvme
+if [ $? == 0 ];then
+    dir=/nvme
+    partition=$1"p1"
+else
+    dir=/notnvme
+    partition=$1"1"
+fi
+
+parted -s $disk mklabel gpt
+parted -s $disk  mkpart primary ext4 1 100%
+parted -s $disk toggle 1 lvm
+
+pvcreate $partition -y
+vgcreate $vgname $partition
