692566a80416561d8ddca4504ab58ef074a803f7
vanvank <165584943@qq.com>
2020-11-05 15:09:06 +0800

    Update install_lotus_20.04.sh

--- install_lotus_20.04.sh
+++ install_lotus_20.04.sh
@@ -8,7 +8,7 @@ sudo bash -c "echo 'export PATH=\$PATH:/usr/local/go/bin' >> /etc/profile"
 
 # install deps
 sudo apt update && \
-sudo apt install mesa-opencl-icd ocl-icd-opencl-dev -y && \
+sudo apt install mesa-opencl-icd ocl-icd-opencl-dev clang -y && \
 ##  sudo add-apt-repository ppa:longsleep/golang-backports -y && \
 sudo apt update && \
 ##  sudo apt install curl golang-go gcc git bzr jq pkg-config  -y && \
