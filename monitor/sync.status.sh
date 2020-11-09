d17dfc1ac897e46e49b4af136b38b0ee6c39de0f
vanvank <165584943@qq.com>
2020-11-09 10:51:01 +0800

    Update sync.status.sh

--- monitor/sync.status.sh
+++ monitor/sync.status.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+node_name="Guizhou lotus node"
 status=`lotus sync status|grep Stage|head -n1|awk '{print $2}'`
 if [ $status == complete ]
 then
-        echo "sync ok"
+        echo "$node_name sync ok"
 else
-        ./send_msg.sh "xueyuan 3700x-1151 sync not ok"
+        ./send_msg.sh "Warning: $node_name sync not ok"
+        sleep 30
+        status=`lotus sync status|grep Stage|head -n1|awk '{print $2}'`
+        if [ $status == complete ];then
+                echo "$node_name sync ok"
+        else
+                ./send_msg.sh "Warning: $node_name sync not ok +1"
+        fi
 fi
