6c01e26a7b6722d1c023bd90b6ff55938e7c5768
vanvank <165584943@qq.com>
2020-12-09 16:33:24 +0800

    Update pledge.sh

--- pledge/pledge.sh
+++ pledge/pledge.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 set -x
 inter=600
-old_proving_count=$(lotus-miner info|grep Proving|tail -n1|awk '{print $2}')
-#old_proving_count=1
-sleep 5
+old_proving_count=$1
+if [ -z $1 ];then
+    old_proving_count=$(lotus-miner info|grep Proving|tail -n1|awk '{print $2}')
+    sleep 5
+fi
 while true
 do
 current_proving_count=$(lotus-miner info|grep Proving|tail -n1|awk '{print $2}')
-need_ap_count=$(( $current_proving_count-$old_proving_count))
+need_ap_count=$((current_proving_count - old_proving_count))
 
 if [ $need_ap_count -gt 0 ];then
   for i in $(seq 1 $need_ap_count);
       do
+      date
       echo "pledge"
-      #lotus-miner sectors pledge
-      sleep 3
+      lotus-miner sectors pledge
+      sleep 10
   done
   old_proving_count=$current_proving_count
 fi
