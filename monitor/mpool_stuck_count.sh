026025cafe7862b7611c8066e431bc5be3c36d42
vanvank <165584943@qq.com>
2020-12-19 21:12:57 +0800

    Update mpool_stuck_count.sh

--- monitor/mpool_stuck_count.sh
+++ monitor/mpool_stuck_count.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -x
+while true
+do
+time=`date`
 count=`lotus mpool pending --local|grep Nonce|wc -l`
-msg="lotus mpool stuck message count: $count"
+msg="$time lotus mpool stuck message count: $count"
 ./send_msg.sh "$msg"
+sleep 1800
+done
