e60f298f46d7382303c19b1229e66d9ea83641cf
vanvank <165584943@qq.com>
2020-12-19 17:49:47 +0800

    Update sync.status.sh

--- monitor/sync.status.sh
+++ monitor/sync.status.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
+set -x
 node_name="Guizhou lotus node"
 status=`lotus sync status|grep Stage|head -n1|awk '{print $2}'`
-if [ $status == complete ]
+is_done=`lotus sync wait|grep Done!`
+diff_count=`lotus sync status|grep diff|head -n1|awk '{print $3}'`
+if [ $status == complete ] || [ $is_done == Done! ] || [ $diff_count -lt 6 ]
 then
-        echo "$node_name sync ok"
+        echo "$(date) $node_name sync ok"
 else
-        ./send_msg.sh "Warning: $node_name sync not ok"
-        sleep 300
-        status=`lotus sync status|grep Stage|head -n1|awk '{print $2}'`
-        if [ $status == complete ];then
-                echo "$node_name sync ok"
-        else
-                ./send_msg.sh "Warning: $node_name sync not ok +1"
-        fi
+        ./send_msg.sh "$(date) Warning: $node_name sync not ok, diff: $diff_count"
 fi
