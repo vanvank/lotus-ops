157604c810a46f0ede2cf26e48f839e7eeb6f44f
vanvank <165584943@qq.com>
2020-12-29 10:59:04 +0800

    Update useful_scripts.sh

--- scheduler/useful_scripts.sh
+++ scheduler/useful_scripts.sh
@@ -1,3 +1,6 @@
+这里记录一些分析调度用的脚本
+
+
 # 这个命令行很有用，可以对 miner 中出现的 out of space 相关的 worker 及次数，进行统计。out of space 是 worker 不能接收任何工作任务的首要因素。
 cat miner.log |grep -a "out of space "|awk '{print $8}'|sort -rn|uniq -c
 
