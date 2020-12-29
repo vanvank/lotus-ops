d1568f13d1a0658edfc02ec6eada9cbd26714959
vanvank <165584943@qq.com>
2020-12-29 10:40:27 +0800

    Update clear_mpool.sh

--- mpool/clear_mpool.sh
+++ mpool/clear_mpool.sh
@@ -1,58 +1,58 @@
 #!/bin/bash
-  2 set -x
-  3 echo "1" > old_nonce.txt
-  4 wallet=f3rislef2hebz4qt6rsmcrentgtn66jelxi2r6t4i7oot4gknwsfihoasfz24kiwqd64k257xldbftgwxnlnba
-  5 t=0
-  6 trigger=50
-  7 stuck_max=$trigger
-  8 while true
-  9 do
- 10 date
- 11 #inter=`cat inter.txt`
- 12 inter=15
- 13 old_nonce=`cat old_nonce.txt`
- 14 count=`lotus mpool pending --local|grep Nonce|wc -l`
- 15 if [ $count -lt $stuck_max ] || [ -z $count ];then
- 16         echo "没有消息卡住"
- 17         date
- 18         stuck_max=$trigger
- 19         #break
- 20         sleep 30
- 21         continue
- 22 else
- 23         stuck_max=1
- 24 fi
- 25
- 26 nonce=`lotus mpool pending --local|grep Nonce|head -n1|awk '{print $2}'|tr -d ,`
- 27
- 28 if [ $nonce == $old_nonce  ]
- 29 then
- 30         sleep 5
- 31         t=$(($t+5))
- 32 else
- 33         premium=$(./mpool.py $wallet)
- 34         echo $premium
- 35         #lotus mpool replace --auto $wallet $nonce
- 36         #lotus mpool replace --gas-feecap 5200000000 --gas-premium $premium --gas-limit 51104935 $wallet  $nonce
- 37         lotus mpool replace --gas-feecap 5200000000 --gas-premium $premium $wallet  $nonce
-            #lotus mpool replace --gas-feecap 100000000000 --gas-premium $premium $wallet  $nonce
- 39         if [ $? == 0 ];then
- 40             echo $nonce > old_nonce.txt
- 41             date
- 42             t=0
- 43             sleep $inter
- 44         else
- 45                 sleep 3
- 46                 continue
- 47         fi
- 48 fi
- 49 echo "t is:$t"
- 50 if [ $t -ge 1200 ]
- 51 then
- 52         echo "消息超过20分钟没上链，触发2次疏通"
- 53         lotus mpool replace --gas-feecap 6200000000 --gas-premium $premium  $wallet  $nonce
- 54         date
- 55         t=0
- 56         sleep $inter
- 57 fi
- 58 done
+set -x
+echo "1" > old_nonce.txt
+wallet=f3rislef2hebz4qt6rsmcrentgtn66jelxi2r6t4i7oot4gknwsfihoasfz24kiwqd64k257xldbftgwxnlnba
+t=0
+trigger=50
+stuck_max=$trigger
+while true
+do
+date
+#inter=`cat inter.txt`
+inter=15
+old_nonce=`cat old_nonce.txt`
+count=`lotus mpool pending --local|grep Nonce|wc -l`
+if [ $count -lt $stuck_max ] || [ -z $count ];then
+        echo "没有消息卡住"
+        date
+        stuck_max=$trigger
+        #break
+        sleep 30
+        continue
+else
+        stuck_max=1
+fi
+
+nonce=`lotus mpool pending --local|grep Nonce|head -n1|awk '{print $2}'|tr -d ,`
+
+if [ $nonce == $old_nonce  ]
+then
+        sleep 5
+        t=$(($t+5))
+else
+        premium=$(./mpool.py $wallet)
+        echo $premium
+        #lotus mpool replace --auto $wallet $nonce
+        #lotus mpool replace --gas-feecap 5200000000 --gas-premium $premium --gas-limit 51104935 $wallet  $nonce
+        lotus mpool replace --gas-feecap 5200000000 --gas-premium $premium $wallet  $nonce
+        #lotus mpool replace --gas-feecap 100000000000 --gas-premium $premium $wallet  $nonce
+        if [ $? == 0 ];then
+            echo $nonce > old_nonce.txt
+            date
+            t=0
+            sleep $inter
+        else
+                sleep 3
+                continue
+        fi
+fi
+echo "t is:$t"
+if [ $t -ge 1200 ]
+then
+        echo "消息超过20分钟没上链，触发2次疏通"
+        lotus mpool replace --gas-feecap 6200000000 --gas-premium $premium  $wallet  $nonce
+        date
+        t=0
+        sleep $inter
+fi
+done
