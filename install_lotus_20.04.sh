6217aa50af2327dcabaca253a57b2da1dfa30334
vanvank <165584943@qq.com>
2021-01-22 17:11:11 +0800

    Update install_lotus_20.04.sh

--- install_lotus_20.04.sh
+++ install_lotus_20.04.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # install go
-#wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-#export PATH=$PATH:/usr/local/go/bin
-#sudo sed -i '/\/usr\/local\/go\/bin/d' /etc/profile
-#sudo bash -c "echo 'export PATH=\$PATH:/usr/local/go/bin' >> /etc/profile"
+go_url=https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz
+wget -c $go_url -O - | sudo tar -xz -C /usr/local
+export PATH=$PATH:/usr/local/go/bin
+sudo sed -i '/\/usr\/local\/go\/bin/d' /etc/profile
+sudo bash -c "echo 'export PATH=\$PATH:/usr/local/go/bin' >> /etc/profile"
+
 
 # install deps
 sudo apt update && \
