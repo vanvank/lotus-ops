8589d6a5f631a54aafc7405492dc1d5bdabee277
vanvank <165584943@qq.com>
2021-01-29 10:43:37 +0800

    Update chainnotifyerror.sh

--- monitor/chainnotifyerror.sh
+++ monitor/chainnotifyerror.sh
@@ -9,11 +9,11 @@ sleep 3
 find_ChainNotify_error2=$(cat $logfile_path|grep wdpost|grep "ChainNotify error:"|tail -n1)
 if [ $? == 0 ];then
         if [ "$find_ChainNotify_error"x == "$find_ChainNotify_error2"x ];then
-            echo $date
+            echo $(date)
             echo "OK! Miner $HOSTNAME connection with node ok"
         else
-            echo $date
+            echo $(date)
             echo "ERROR lotus connection"
-            ./send_msg.sh "ERROR!!! Miner $HOSTNAME lost connection with node. $date"
+            ./send_msg.sh "ERROR!!! Miner $HOSTNAME lost connection with node. $(date)"
         fi
 fi
