0203f8ef0f8c930a760041f45482b996be821382
vanvank <165584943@qq.com>
2021-01-29 10:42:22 +0800

    Update chainnotifyerror.sh

--- monitor/chainnotifyerror.sh
+++ monitor/chainnotifyerror.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 这个脚本监控miner日志中出现的ChainNotify error这个问题。可以放到crontab中执行。
+# */10 * * * * cd /opt/lotus-ops/monitor; ./chainnotifyerror.sh >> log.chainnotifyerror
+
 set -x
 logfile_path="/home/xl/miner.log"
 find_ChainNotify_error=$(cat $logfile_path|grep wdpost|grep "ChainNotify error:"|tail -n1)
@@ -6,9 +9,11 @@ sleep 3
 find_ChainNotify_error2=$(cat $logfile_path|grep wdpost|grep "ChainNotify error:"|tail -n1)
 if [ $? == 0 ];then
         if [ "$find_ChainNotify_error"x == "$find_ChainNotify_error2"x ];then
+            echo $date
             echo "OK! Miner $HOSTNAME connection with node ok"
         else
+            echo $date
             echo "ERROR lotus connection"
-            ./send_msg.sh "ERROR!!! Miner $HOSTNAME lost connection with node"
+            ./send_msg.sh "ERROR!!! Miner $HOSTNAME lost connection with node. $date"
         fi
 fi
