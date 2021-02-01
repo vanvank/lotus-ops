2961aad6cf2263028afa4a324376952a5b413b5b
vanvank <165584943@qq.com>
2021-02-01 18:36:00 +0800

    Update chainnotifyerror.sh

--- monitor/chainnotifyerror.sh
+++ monitor/chainnotifyerror.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 # 这个脚本监控miner日志中出现的ChainNotify error这个问题。可以放到crontab中执行。
 # */10 * * * * cd /opt/lotus-ops/monitor; ./chainnotifyerror.sh >> log.chainnotifyerror
-
 set -x
+
+echo "start******"
+source /etc/profile
+
 logfile_path="/home/xl/miner.log"
 find_ChainNotify_error=$(cat $logfile_path|grep wdpost|grep "ChainNotify error:"|tail -n1)
+result=$(/usr/local/bin/lotus-miner info)
+if [ $? != 0 ];then
+        ./send_msg.sh "ERROR!!! Miner $HOSTNAME info not ok, maybe lost connection with node. $(date)"
+        echo  "ERROR!!! Miner $HOSTNAME info not ok, maybe lost connection with node. $(date)"
+fi
 sleep 3
 find_ChainNotify_error2=$(cat $logfile_path|grep wdpost|grep "ChainNotify error:"|tail -n1)
 if [ $? == 0 ];then
         if [ "$find_ChainNotify_error"x == "$find_ChainNotify_error2"x ];then
             echo $(date)
+            #./send_msg.sh "OK! Miner $HOSTNAME connection with node ok"
             echo "OK! Miner $HOSTNAME connection with node ok"
         else
             echo $(date)
@@ -17,3 +26,4 @@ if [ $? == 0 ];then
             ./send_msg.sh "ERROR!!! Miner $HOSTNAME lost connection with node. $(date)"
         fi
 fi
+echo "end********"
