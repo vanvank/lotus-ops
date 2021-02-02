a3be99fd59aa1391429f39e053c4c892e228e0aa
vanvank <165584943@qq.com>
2021-02-02 16:37:06 +0800

    Update sync.status.sh

--- monitor/sync.status.sh
+++ monitor/sync.status.sh
@@ -7,7 +7,7 @@ node_name=$HOSTNAME
 status=`lotus sync status|grep Stage|head -n1|awk '{print $2}'`
 is_done=`timeout 5 lotus sync wait|grep Done!`
 diff_count=`lotus sync status|grep diff|head -n1|awk '{print $3}'`
-if [ $status == complete ] || [ $is_done == Done! ] || [ $diff_count -lt 6 ]
+if [ $status != error ] || [ $is_done == Done! ] || [ $diff_count -lt 6 ]
 then
         echo "$(date) $node_name sync ok"
 else
