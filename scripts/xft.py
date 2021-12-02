d20c3c6f888c48e626232d186d70128d3cf2c169
vanvank <165584943@qq.com>
2021-12-02 10:17:51 +0800

    Update xft.py

--- scripts/xft.py
+++ scripts/xft.py
@@ -1,34 +1,42 @@
 from decimal import Decimal
 
-# 8/25 链上收益0.0346/T/天
-current_chain_profit = 0.0346
-left_xft = 777310
+# 链上收益0.0228/T/天
+current_chain_profit = 0.0228
 total_xft = 1024 * 1024
 
-
-# 总共需要补充进池子的FIL数量
-transfer = （total_xft - left_xft） / 1024 * current_chain_profit * 0.4 * 7
+left_xft = 767570
+sold_xft = total_xft - left_xft
+days = 14
+seconds = days * 24 * 3600
 
 # 提高精度，不要显示科学计数，精确到小数点后18个0
 def print_decimal(f):
     f = Decimal.from_float(f * 1000000000000000000)
     print(f)
 
+# 每TiB算力每小时奖励
+print("收益周期：%s" % seconds  )
+print("40TB每小时收益：")
+print(current_chain_profit*0.4*40*100/103/24)
+print("40TB每天收益：")
+print(current_chain_profit*0.4*40*100/103)
+print("40TB每分钟收益： ")
+print(current_chain_profit*0.4*40*100/103/24/60)
+print(current_chain_profit*0.4*40*100/103/24/60)
 
+# 总共需要补充进池子的FIL数量
+transfer = sold_xft / 1024 * current_chain_profit * 0.4 * days
 
-
-# 两个池子总共需要发放的数量
-total = current_chain_profit * (total_xft / 1024) * 0.4 * 7
+# 总共需要发放的数量
+total = current_chain_profit * (total_xft / 1024) * 0.4 * days
 print("总共需要发的：%s" % total)
 print_decimal(total)
 
-# 各池子本期需要发放的FIL数量
-xft_pool = total*100/103
-xfti_pool = total*3/103
+xft_pool = total * 100 / 103
+xfti_pool = total * 3 / 103
 
-# 池子实际需要补充的FIL
-xft = transfer * 100/103
-xfti = transfer * 3/103
+xft = transfer * 100 / 103
+xfti = transfer * 3 / 103
 
 print()
 print("xft池子收益")
