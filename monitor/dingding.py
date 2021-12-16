67815a1b370555e0e8e9479c2a1a463e9fffcf2f
vanvank <165584943@qq.com>
2021-12-16 17:16:59 +0800

    Update dingding.py

--- monitor/dingding.py
+++ monitor/dingding.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
-#coding:utf-8
-#zabbix钉钉报警
-import requests,json,sys,os,datetime
-webhook="https://oapi.dingtalk.com/robot/send?access_token=XXXXXXXXXX"
-user=sys.argv[1]
-text=sys.argv[3]
-data={
-    "msgtype": "text",
-    "text": {
-        "content": text
-    },
-    "at": {
-        "atMobiles": [
-            user
-        ],
-        "isAtAll": False
+# coding:utf-8
+# zabbix钉钉报警
+
+import requests
+import json
+import sys
+
+def ding_msg(user,title,text):
+    webhook = "https://oapi.dingtalk.com/robot/send?access_token=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+    data={
+        "msgtype": "text",
+        "text": {
+            "content": text
+        },
+        "at": {
+            "atMobiles": [
+                user
+            ],
+            "isAtAll": False
+        }
     }
-}
-headers = {'Content-Type': 'application/json'}
-x=requests.post(url=webhook,data=json.dumps(data),headers=headers)
+    headers = {'Content-Type': 'application/json'}
+    x=requests.post(url=webhook,data=json.dumps(data),headers=headers)
+
+if __name__ == '__main__':
+    user=sys.argv[1]
+    text=sys.argv[3]
+    title = "zabbix-alert"
+    print("test")
+    ding_msg(user,title,text)
+    #ding_msg('a', 'b', 'xmall:test')
